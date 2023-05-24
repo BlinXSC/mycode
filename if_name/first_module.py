1b9617e11f24632e6c53857bbc63d4c801e6cfa3
BlinXSC <alanmark.alegre@gmail.com>
2023-05-24 22:16:21 +0000

    Playing with Python pip packages

---
+++ if_name/first_module.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+print("This code will always execute.")
+
+def greeting():
+    print("WAZZZZZZZAP!!!")
+
+def main():
+   print("This code belongs to the main function in module 1.")
+
+if __name__ == "__main__":
+   main()

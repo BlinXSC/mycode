bb357530ce7980d94ca29880e51abebede0e3867
BlinXSC <alanmark.alegre@gmail.com>
2023-05-25 17:00:47 +0000

    Challenge complete

--- challenges/99_bottles_of_beer.py
+++ challenges/99_bottles_of_beer.py
@@ -3,16 +3,27 @@
 
 def main():
 
-    # Provide the amount of bottles of beer on the wall.
-    bottles_of_beer = input("Please provide the number of beer bottles we will be drinking today >>> ")
+    # Initializes bottles of beer
+    bottles_of_beer = 101
 
-    # Ensures user provides a number.
-    while True:
+    # Ensures user provides a integer to represent number of bottles of beer to drink, no more than 100
+    while bottles_of_beer > 100:
         try:
             bottles_of_beer = int(input("Please provide the number of beer bottles we will be drinking today >>> "))
-            break
+            if bottles_of_beer > 100:
+                print("Too much beer... enter no more than 100.")
         except:
             print("You did not provide a number")
+
+    # Creates a range for a for loop
+    drink = range(bottles_of_beer, 0, -1)
+
+    # For loop constructs the song
+    for x in drink:
+        print(f"\n{x} bottles of beer on the wall")
+        print(f"{x} bottles of beer")
+        print("Take one down, pass it around")
+        print(f"{x - 1} bottles of beer on the wall")
 
 
 main()

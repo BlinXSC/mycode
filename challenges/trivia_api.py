76e27baa15cd1c532b9aa1a4432a6b8f7e3d3471
BlinXSC <alanmark.alegre@gmail.com>
2023-06-01 21:14:58 +0000

    My first Flask apps

--- challenges/trivia_api.py
+++ challenges/trivia_api.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python3
 """Friday Warmup | Returning Data From Complex JSON"""
 
-# Provides the appropriate 
+# Provides the appropriate
 import html
 import random
 import requests
+from textwrap import dedent
 
-URL= "https://opentdb.com/api.php?amount=3&difficulty=easy"
+# Default URL
+URL= "https://opentdb.com/api.php?amount=5&difficulty=easy"
+
+def trivia_modifier():
+    """ Allows user to modify difficulty """
+    # Initializes user choices
+    user_difficulty = ''
+    question_count = 0
+
+    # Creates a list with difficulties
+    difficulty = ['easy', 'medium', 'hard']
+    
+    while user_difficulty not in difficulty:
+        user_difficulty = input(dedent(
+            """
+            Please select how challenging you want this trivia to be:
+
+            Easy
+            Medium
+            Hard
+
+            >>> """)).lower()
+
+        if user_difficulty not in difficulty:
+            print("\n***Invalid option, please select again***")
 
 def main():
     "Executes the trivia program"
 
     print("Welcome to the trivia game!!!") # Update
+
+    # Prompts the user to select a difficulty and amount of questions.
+    trivia_modifier()
 
     # Initialize question counters and appropriate trackers
     counter = 0
@@ -58,11 +86,14 @@ def main():
         counter += 1
 
         # Checks for correct answer
-        if answer_dict[user_answer] == correct_answer:
-            print("Correct!!!\n")
-            correct_selections += 1
-        else:
-            print("Incorrect\n")
+        try:
+            if answer_dict[user_answer] == correct_answer:
+                print("Correct!!!\n")
+                correct_selections += 1
+            else:
+                print("Incorrect\n")
+        except:
+            print("Need to select A, B, C, or D. This is be marked incorrect")
 
         # Scores the trivia
         score = int((correct_selections / counter) * 100)

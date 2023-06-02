6a4102a087d719ae8ff60b782f7c287c876d8a18
BlinXSC <alanmark.alegre@gmail.com>
2023-06-02 02:43:18 +0000

    First draft, potential final project

--- tlg_project/trivia_api.py
+++ tlg_project/trivia_api.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
-"""Friday Warmup | Returning Data From Complex JSON"""
+"""Alan Alegre | Final Project: Trivia Game"""
 
 # Provides the appropriate
 import html
 import random
-import requests
 from textwrap import dedent
+import requests
 
-# Default URL
-URL= "https://opentdb.com/api.php?amount=5&difficulty=easy"
-
-def trivia_modifier():
+def trivia_modifier(url):
     """ Allows user to modify difficulty """
 
     # Creates a list with difficulties
     difficulty = ['easy', 'medium', 'hard']
-    
+
     # Initializes confirmation variable
     confirmation = ''
 
@@ -41,7 +38,7 @@ def trivia_modifier():
             if user_difficulty not in difficulty:
                 print("\n***Invalid option, please select again***")
 
-        # Ensures the user provides an integer greater than 5.     
+        # Ensures the user provides an integer greater than 5.
         while question_count < 5:
             try:
                 question_count = int(input("\nHow many questions? >>> "))
@@ -50,13 +47,13 @@ def trivia_modifier():
                 if question_count < 5:
                     print("\nFive questions is the minimum required to start the game.")
 
-                # Pokes fun at the user if they enter 50 or more questions. 
+                # Pokes fun at the user if they enter 50 or more questions.
                 if question_count > 50:
                     print("\nJust how much spare time do you have???")
-            except:
+            except TypeError:
                 print("\nPlease provide an integer.")
 
-        # Confirm user choice, restarts while loop if user does not 
+        # Confirm user choice, restarts while loop if user does not
         print(f"\nDifficulty: {user_difficulty.capitalize()}")
         print(f"Number of questions: {question_count}")
         confirmation = input("Please enter \'Y\' to confirm your selection >>> ").capitalize()
@@ -65,8 +62,10 @@ def trivia_modifier():
             # Converts question_count to a string object
             question_count = str(question_count)
 
-        # Provides the link to extract the API from the trivia site.
-        URL = f"https://opentdb.com/api.php?amount={question_count}&difficulty={user_difficulty}"
+    # Provides the link to extract the API from the trivia site.
+    url = f"https://opentdb.com/api.php?amount={question_count}&difficulty={user_difficulty}"
+
+    return url
 
 def main():
     "Executes the trivia program"
@@ -84,24 +83,25 @@ def main():
         C. Answer 3
         D. Answer 4
 
-        Hope you do well, otherwise the program will question your overall 
-        intelligence. All in good fun, please enjoy!!!""")) # Update
+        Hope you do well, otherwise the program will question your intelligence
+        All in good fun, please enjoy!!!""")) # Update
 
     # Prompts the user to select a difficulty and amount of questions.
-    trivia_modifier()
+    url = ''
+    url = trivia_modifier(url)
 
     # Initialize question counters and appropriate trackers
     counter = 0
     correct_selections = 0
 
     # Data will be a python dictionary rendered from your API link's JSON
-    data = requests.get(URL, timeout=10).json()
+    data = requests.get(url, timeout=10).json()
 
     # Print questions
     for question in data['results']:
 
         # Decodes any remaining HTML code in the dictionary.
-        print(f"\n*****Question {counter + 1}: {html.unescape(question['question'])}*****\n")
+        print(f"\n***** Question {counter + 1}: {html.unescape(question['question'])} *****\n")
 
         # Creates the list of questions
         answers = []
@@ -123,9 +123,9 @@ def main():
         i = 65
 
         # Print answers
-        for x in answers:
-            print(f"{chr(i)}. {x}")
-            answer_dict[chr(i)] = x
+        for line in answers:
+            print(f"{chr(i)}: {line}")
+            answer_dict[chr(i)] = line
             i += 1
 
         # Prompts the user for input
@@ -137,15 +137,28 @@ def main():
         # Checks for correct answer
         try:
             if answer_dict[user_answer] == correct_answer:
-                print("Correct!!!\n")
+                print("\nCorrect!!!")
                 correct_selections += 1
             else:
-                print("Incorrect\n")
-        except:
+                print(f"\nIncorrect. The correct answer is {correct_answer}")
+        except KeyError:
             print("Need to type A, B, C, or D. This will be marked incorrect")
 
         # Scores the trivia
         score = int((correct_selections / counter) * 100)
+
+    # Prints the score, and informs the user if they passed or not.
+    print("Passing score is 50.")
+    print(f"\nFinal score: {score}")
+    if score > 50:
+        print(dedent("""
+            Great job. The easy mode is challenging as it is. It is time to push yourself
+            and select the harder modes. Major props if you selected the harder difficulties. 
+            """))
+    elif score > 0:
+        print("\nPerhaps you need to study up a bit.")
+    else:
+        print("\nYou couldn't even get one right, Private Pyle?")
 
 if __name__ == "__main__":
     main()
